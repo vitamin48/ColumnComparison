9145c8b823875082265cb7f89c045f571348fa3d
Компьютер <vitamin48>
2024-03-21 18:54:14 +0300

    Доделана разница

--- column_comparison.py
+++ column_comparison.py
@@ -4,28 +4,45 @@
 """
 import csv
 
-# Открываем файл CSV для чтения
-with open('1.csv', newline='', encoding='utf-8') as csvfile:
-    # Создаем объект чтения CSV
-    csv_reader = csv.reader(csvfile)
-    dict_csv = csv.DictReader(csvfile)
-    sgtin_lst = []
-    # Читаем данные строку за строкой
-    for row in dict_csv:
-        # row представляет каждую строку в файле CSV как список значений
-        sgtin_lst.append(row['sgtin'])
+NAME_RESULT_FILE = '240321/дигоксин_difference.txt'
 
-with open('240314\\Коды из ЧестнЗнака.txt', 'r', encoding='utf-8') as file:
-    lines = [f'{line}'.rstrip() for line in file]
 
-with open('240314\\Коды сканир с Упаковок.txt', 'r', encoding='utf-8') as file:
-    lines3 = [f'{line}'.rstrip() for line in file]
+def read_csv():
+    # Открываем файл CSV для чтения
+    with open('1.csv', newline='', encoding='utf-8') as csvfile:
+        # Создаем объект чтения CSV
+        csv_reader = csv.reader(csvfile)
+        dict_csv = csv.DictReader(csvfile)
+        sgtin_lst = []
+        # Читаем данные строку за строкой
+        for row in dict_csv:
+            # row представляет каждую строку в файле CSV как список значений
+            sgtin_lst.append(row['sgtin'])
+        return sgtin_lst
+
+
+with open('Коды_честный_знак.txt', 'r', encoding='utf-8') as file:
+    znak_lst = [f'{line}'.rstrip() for line in file]
+
+with open('Коды_сканер.txt', 'r', encoding='utf-8') as file:
+    scaner_lst = [f'{line}'.rstrip() for line in file]
 
 
 def find_full_intersection(list1, list2):
     return list(set(list1) & set(list2))
 
-lines4 = [f'{x[2:16]}{x[18:31]}' for x in lines3]
 
-intersection = find_full_intersection(lines, lines4)
-print()
+def find_difference(list1, list2):
+    return list(set(list1).difference(set(list2)))
+
+
+lines4 = [f'{x[2:16]}{x[18:31]}' for x in scaner_lst]
+
+intersection = find_full_intersection(znak_lst, lines4)
+difference = find_difference(znak_lst, lines4)
+
+print(difference)
+print(len(difference))
+
+with open(NAME_RESULT_FILE, 'a') as file:
+    file.writelines(diff + '\n' for diff in difference)
